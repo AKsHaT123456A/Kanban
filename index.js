4c1486f7eb69ecd6fb19e2d3f2a313cb3737af2b
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-10-10 18:39:31 +0530

    Feat:Project Uploaded

---
+++ index.js
@@ -0,0 +1,52 @@
+require('colors');
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('./middleware/error/errorHandler');
+const port = process.env.PORT || 5000;
+
+// Connect DB
+connectDB();
+
+// Init express app
+const app = express();
+
+// Initialize cookie parser
+app.use(cookieParser());
+
+// Resolve cors
+const corsOptions = {
+	origin: 'https://kanban-task-management-six.vercel.app', 
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
+
+// Init middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Define routes
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/boards', require('./routes/boardRoutes'));
+app.use('/api/columns', require('./routes/columnRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
+
+// Error handler middleware
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/dist')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
+	});
+}
+app.listen(port, () => {
+	console.log(`Server successfully started on port ${port} 🚀`);
+});
